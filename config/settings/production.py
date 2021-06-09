837f230cda64e7138ff602468575868bf422b68f
Caos <caosbad@gmail.com>
2021-06-09 10:57:16 +0800

    feat: update kcc config

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,12 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['gnosis.pm'])
+ALLOWED_HOSTS = [
+    "localhost",
+    "13.208.111.206",
+    "172.31.47.188",
+    "127.0.0.1"
+]
 
 # DATABASES
 # ------------------------------------------------------------------------------
